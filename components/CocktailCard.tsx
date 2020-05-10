3f7fdd785ff9c828bebf37f255233402a8faccdd
Lim Tae Hyun <dlatns0201@naver.com>
2020-05-10 19:33:20 +0900

    feat: write cocktail card html markup
    index page에 임의의 데이터를 넣어서 테스트

--- components/CocktailCard.tsx
+++ components/CocktailCard.tsx
@@ -1,5 +1,46 @@
-const CocktailCard = () => {
-  return <div>CocktailCard</div>;
+import Link from 'next/link';
+import styled from 'styled-components';
+
+interface IImageInfo {
+  src: string;
+  alt: string;
+}
+
+interface IProps {
+  imageInfo: IImageInfo;
+  cocktailID: number;
+  alcoholDegree: number;
+  cocktailName: string;
+  ingredients: string[];
+}
+
+const StyledCard = styled.div``;
+const InfoBlock = styled.div``;
+
+const CocktailCard = ({
+  imageInfo,
+  cocktailID,
+  alcoholDegree,
+  cocktailName,
+  ingredients,
+}: IProps) => {
+  return (
+    <StyledCard>
+      <Link
+        href={{ pathname: '/cocktaildetail', query: { cocktailID } }}
+        as={`/cocktaildetail/${cocktailID}`}
+      >
+        <a>
+          <img src={imageInfo.src} alt={imageInfo.alt} />
+          <InfoBlock>
+            <p>{cocktailName}</p>
+            <p>{ingredients}</p>
+            <p className="alcohol-degree">{alcoholDegree}</p>
+          </InfoBlock>
+        </a>
+      </Link>
+    </StyledCard>
+  );
 };
 
 export default CocktailCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
+import CocktailCard from '../components/CocktailCard';
+
 interface IProps {}
 
 const IndexPage = (props: IProps) => {
-  return <div>Index Page</div>;
+  return (
+    <div>
+      <CocktailCard
+        imageInfo={{
+          src:
+            'https://lh3.googleusercontent.com/proxy/UPfo-MYtzksVp8pZKNqNPpTcUo7QtSyyJ8XPJh4axZEj0mbY9BawG7CBuqiGI8_LgGN7-2MQ0y1motb4AWnpjKkLc25RIaRz-G0IZG9V7lmMQxJ6OL7MwoozqUzGaNbUYHwi5hnrhZ_7RT94Jb4-l02fOQrpwCaJWENO_A',
+          alt: '블랙 러시안',
+        }}
+        alcoholDegree={20}
+        cocktailName={'블랙 러시안'}
+        cocktailID={1}
+        ingredients={['깔루아', '보드카']}
+      />
+    </div>
+  );
 };
 
 export default IndexPage;
