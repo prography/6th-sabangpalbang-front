76911916cf27daa6dd777fd330732240e1bb41fb
Lim Tae Hyun <dlatns0201@naver.com>
2020-05-24 01:40:09 +0900

    feat: index 페이지의 더미 데이터 이동
    carousel 명칭 변경한 것 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Banner from '../components/Banner';
-import BannerCarousel from '../components/BannerCarousel';
+import Carousel from '../components/Carousel';
 import CocktailCard from '../components/CocktailCard';
 import CocktailGrid from '../components/CocktailGrid';
 import FilterTab from '../components/FilterTab';
 import TopNavigation from '../components/TopNavigation';
+import * as dummy from '../config/dummy';
 
 interface IProps {}
 
@@ -11,78 +12,8 @@ const IndexPage = (props: IProps) => {
   return (
     <>
       <TopNavigation />
-      <BannerCarousel
-        BannerComponent={Banner}
-        BannerInfos={[
-          { src: 'banner_temp.jpeg', alt: '', href: '' },
-          { src: 'banner_temp2.jpg', alt: '', href: '' },
-          { src: 'banner_temp3.jpeg', alt: '', href: '' },
-        ]}
-      />
-      <FilterTab
-        filters={[
-          {
-            category: '베이스',
-            filterList: [
-              {
-                href: '',
-                filterName: '위스키',
-                filterImage: { src: '위스키.png', alt: '' },
-              },
-              {
-                href: '',
-                filterName: '럼',
-                filterImage: { src: '럼.jpg', alt: '' },
-              },
-              {
-                href: '',
-                filterName: '진',
-                filterImage: { src: '진.jpeg', alt: '' },
-              },
-            ],
-          },
-          {
-            category: '도수',
-            filterList: [
-              {
-                href: '',
-                filterName: '무알콜',
-                filterImage: { src: '위스키.png', alt: '' },
-              },
-              {
-                href: '',
-                filterName: '0~20%',
-                filterImage: { src: '럼.jpg', alt: '' },
-              },
-              {
-                href: '',
-                filterName: '20~40%',
-                filterImage: { src: '진.jpeg', alt: '' },
-              },
-            ],
-          },
-          {
-            category: '태그',
-            filterList: [
-              {
-                href: '',
-                filterName: '19',
-                filterImage: { src: '위스키.png', alt: '' },
-              },
-              {
-                href: '',
-                filterName: '바나나',
-                filterImage: { src: '럼.jpg', alt: '' },
-              },
-              {
-                href: '',
-                filterName: '복숭아',
-                filterImage: { src: '진.jpeg', alt: '' },
-              },
-            ],
-          },
-        ]}
-      />
+      <Carousel ItemComponent={Banner} Infos={dummy.banner} />
+      <FilterTab filters={dummy.filterTab} />
       <CocktailGrid
         CocktailCardComponent={CocktailCard}
         cocktailInfos={[
