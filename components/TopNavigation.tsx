ece3a8b6da9b9ad11949c2e07d5d58052b2fb9b4
Lim Tae Hyun <dlatns0201@naver.com>
2020-05-24 00:41:14 +0900

    style: 네비게이션 스타일 변경
    루트 요소 명을 Bar -> NavContainer로 변경
    theme를 Hoc 방식에서 hooks로 가져오게 변경
    Link 태그안에 불 필요한 div 제거
    스타일을 줄 때 요소 선택자는 성능 상 좋지 않음 클래스 지정방식으로 스타일을 주입해야함
    불 필요한 스타일 제거
    아직 동적으로 클릭 효과를 주지 않음 -> redux 구축 후 변경해야함

--- components/TopNavigation.tsx
+++ components/TopNavigation.tsx
@@ -1,60 +1,51 @@
-import styled, { ThemeConsumer } from 'styled-components';
 import Link from 'next/link';
+import styled, { useTheme } from 'styled-components';
 
-const Bar = styled.div`
-  width: 100%;
+import { ITheme } from '../style';
+
+const NavContainer = styled.nav`
+  position: fixed;
+  top: 44px;
+  right: 0;
+  left: 0;
   height: 44px;
-  ${(props: { themeColor: string; secondThemeColor: string }) =>
+  ${(props: ITheme) =>
     `background: linear-gradient(to right, ${props.themeColor}, ${props.secondThemeColor});`}
   display: flex;
+  justify-content: space-around;
   align-items: center;
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
-  div {
-    flex: 1;
-    text-align: center;
-  }
-  a {
+
+  .nav_item {
     display: inline-block;
     padding: 0 5px;
-    color: white;
+    color: #fff;
     font-size: 18px;
     font-weight: bold;
     line-height: 44px;
-    height: 44px;
+
     &.active {
       border-bottom: 3px solid white;
+      line-height: 40px;
     }
   }
 `;
 
-interface IProps {
-  page: number;
-}
+const TopNavigation = () => {
+  const theme = useTheme() as ITheme;
 
-// 페이지 어딘지 알아내서 체크해야함
-const TopNavigation = ({ page }: IProps) => {
   return (
-    <ThemeConsumer>
-      {(theme) => (
-        <Bar {...theme}>
-          <Link href={`/`}>
-            <div>
-              <a className={page == 0 ? 'active' : ''}>홈</a>
-            </div>
-          </Link>
-          <Link href={`/ranking`}>
-            <div>
-              <a className={page == 1 ? 'active' : ''}>랭킹</a>
-            </div>
-          </Link>
-          <Link href={`/mypage`}>
-            <div>
-              <a className={page == 2 ? 'active' : ''}>마이페이지</a>
-            </div>
-          </Link>
-        </Bar>
-      )}
-    </ThemeConsumer>
+    <NavContainer {...theme}>
+      <Link href='/'>
+        <a className='nav_item active'>홈</a>
+      </Link>
+      <Link href='/ranking'>
+        <a className='nav_item'>랭킹</a>
+      </Link>
+      <Link href='/mypage'>
+        <a className='nav_item'>마이페이지</a>
+      </Link>
+    </NavContainer>
   );
 };
 
