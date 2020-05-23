1de6c16d6ab1848dbc37cac53552f8300757e683
Lim Tae Hyun <dlatns0201@naver.com>
2020-05-24 00:34:29 +0900

    feat: 스타일 관련 정보들을 ./style.ts 로 빼냄
    글로벌 스타일과 reset css 이동
    theme 정보 이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,103 +1,23 @@
+import 'swiper/css/swiper.css';
+
 import withRedux from 'next-redux-wrapper';
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import {
-  applyMiddleware,
-  compose,
-  createStore,
-  Middleware,
-  Store,
-} from 'redux';
+import { applyMiddleware, compose, createStore, Middleware, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createEpicMiddleware } from 'redux-observable';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import 'swiper/css/swiper.css';
 
 import rootEpic from '../epics';
 import rootReducer from '../reducers';
+import { globalStyle, resetCSS, theme } from '../style';
 
 interface IProps extends AppProps {
   store: Store;
 }
 
-const ResetCSS = createGlobalStyle`
-::-webkit-scrollbar{-webkit-appearance:none; display:none}
-::-webkit-scrollbar:vertical{width:0}
-::-webkit-scrollbar:horizontal{height:0}
-::-webkit-scrollbar-thumb,
-::-webkit-scrollbar-track{background-color:transparent}
-
-html,body,div,span,h1,h2,h3,h4,h5,h6,p,blockquote,pre,
-a,abbr,address,cite,code,
-del,em,img,ins,q,strong,i
-dl,dt,dd,ol,ul,li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 16px;
-    font: inherit;
-    vertical-align: baseline;
-}
-
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-    display: block;
-}
-
-body {
-    line-height: 1;
-}
-
-ol,ul {
-    list-style: none;
-}
-
-blockquote,q {
-    quotes: none;
-}
-
-blockquote:before, blockquote:after,
-q:before, q:after {
-    content: '';
-    content: none;
-}
-
-table {
-    border-collapse: collapse;
-    border-spacing: 0;
-}
-`;
-const theme = {
-  themeColor: '#FF306F',
-  secondThemeColor: '#FF6C26',
-  primaryTextColor: '#121212',
-  secondTextColor: '#7E7E7E',
-};
-
-const GlobalStyle = createGlobalStyle`
-* {
-  box-sizing: border-box;
-  letter-spacing: normal;
-}
-body{
-  background-color:#E0E0E0;
-}
-a {
-  color: #000;
-  text-decoration: none;
-}
-a:hover {
-  color:#00A0C6; 
-  text-decoration:none; 
-  cursor:pointer;  
-}
-`;
+const ResetCSS = createGlobalStyle`${resetCSS}`;
+const GlobalStyle = createGlobalStyle`${globalStyle}`;
 
 const App = ({ store, Component, pageProps }: IProps) => {
   return (
