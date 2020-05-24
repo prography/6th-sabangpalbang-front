df4288c229e70bdc7f1d980c050ca9f3d24094ea
Lim Tae Hyun <dlatns0201@naver.com>
2020-05-24 13:01:37 +0900

    chore: 칵테일 카드 리스트 더미 데이터를 빼냄

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,74 +16,7 @@ const IndexPage = (props: IProps) => {
       <FilterTab filters={dummy.filterTab} />
       <CocktailGrid
         CocktailCardComponent={CocktailCard}
-        cocktailInfos={[
-          {
-            src:
-              'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_blue_hawaiian-1.png',
-            alt: '',
-            href: '',
-            name: '블루 하와이',
-            tags: [
-              { text: '럼', backgroundColor: '#7CB587', href: '' },
-              { text: '10~15%', backgroundColor: '#7CA1B5', href: '' },
-            ],
-          },
-          {
-            src:
-              'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_blue_hawaiian-1.png',
-            alt: '',
-            href: '',
-            name: '블루 하와이',
-            tags: [
-              { text: '럼', backgroundColor: '#7CB587', href: '' },
-              { text: '10~15%', backgroundColor: '#7CA1B5', href: '' },
-            ],
-          },
-          {
-            src:
-              'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_blue_hawaiian-1.png',
-            alt: '',
-            href: '',
-            name: '블루 하와이',
-            tags: [
-              { text: '럼', backgroundColor: '#7CB587', href: '' },
-              { text: '10~15%', backgroundColor: '#7CA1B5', href: '' },
-            ],
-          },
-          {
-            src:
-              'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_blue_hawaiian-1.png',
-            alt: '',
-            href: '',
-            name: '블루 하와이',
-            tags: [
-              { text: '럼', backgroundColor: '#7CB587', href: '' },
-              { text: '10~15%', backgroundColor: '#7CA1B5', href: '' },
-            ],
-          },
-          {
-            src:
-              'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_blue_hawaiian-1.png',
-            alt: '',
-            href: '',
-            name: '블루 하와이',
-            tags: [
-              { text: '럼', backgroundColor: '#7CB587', href: '' },
-              { text: '10~15%', backgroundColor: '#7CA1B5', href: '' },
-            ],
-          },
-          {
-            src:
-              'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_blue_hawaiian-1.png',
-            alt: '',
-            href: '',
-            name: '블루 하와이',
-            tags: [
-              { text: '럼', backgroundColor: '#7CB587', href: '' },
-              { text: '10~15%', backgroundColor: '#7CA1B5', href: '' },
-            ],
-          },
-        ]}
+        cocktailInfos={dummy.mainCocktailListInfo}
       />
     </>
   );
