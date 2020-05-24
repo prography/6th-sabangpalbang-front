076dbe2c81c8350983ede9bce0f719a8dcda6e16
Lim Tae Hyun <dlatns0201@naver.com>
2020-05-24 18:14:03 +0900

    feat: 메인 페이지 배너에 banner reducer, epic 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,29 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Banner from '../components/Banner';
 import Carousel from '../components/Carousel';
 import CocktailCardList from '../components/CocktailCardList';
 import FilterTab from '../components/FilterTab';
 import TopNavigation from '../components/TopNavigation';
 import * as dummy from '../config/dummy';
+import { RootState } from '../reducers';
+import { bannerRequest } from '../reducers/carousel';
 
 interface IProps {}
 
 const IndexPage = () => {
+  const dispatch = useDispatch();
+  const { banner } = useSelector((state: RootState) => state.carousel);
+
+  useEffect(() => {
+    dispatch(bannerRequest());
+  }, []);
+
   return (
     <>
       <TopNavigation />
-      <Carousel ItemComponent={Banner} Infos={dummy.banner} />
+      <Carousel ItemComponent={Banner} infos={banner} />
       <FilterTab filters={dummy.filterTab} />
       <CocktailCardList cocktailInfos={dummy.mainCocktailListInfo} />
     </>
