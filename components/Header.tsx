2fd550beb39eebbcdadf6c71a7af1519e7ff992a
seokh1213 <seokh1213@gmail.com>
2020-05-29 17:57:02 +0900

    chore: 머지중...

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,22 +23,15 @@ const HeaderContainer = styled.header`
     width: 24px;
   }
 
-<<<<<<< HEAD
   &::after {
     content: '';
     display: block;
     height: 44px;
   }
 `;
-=======
-interface IProps {
-  display?: string;
-}
->>>>>>> master
 
 const Header = () => {
   return (
-<<<<<<< HEAD
     <HeaderContainer>
       <div className='fixed_div'>
         <Link href='/'>
@@ -47,12 +40,6 @@ const Header = () => {
         <img className='search_logo' src='search.png' alt='검색하기' />
       </div>
     </HeaderContainer>
-=======
-    <Bar style={{ display }}>
-      <img style={{ width: '40px' }} src="logo.png" alt="CocktanderLogo" />
-      <img style={{ width: '24px' }} src="search.png" alt="SearchIcon" />
-    </Bar>
->>>>>>> master
   );
 };
 
