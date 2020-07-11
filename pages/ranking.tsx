ebcebca71dc9d057c6a26d14fcbb186b8517e5c1
Lim Tae Hyun <dlatns0201@naver.com>
2020-07-11 13:09:24 +0900

    feat: 랭킹 페이지 리다이렉션 오류 수정

--- pages/ranking.tsx
+++ pages/ranking.tsx
@@ -1,7 +1,14 @@
 import router from 'next/router';
+import { useEffect } from 'react';
 
 const Ranking = () => {
-    router.push('/login');
+    useEffect(() => {
+        router.push('/login');
+    }, []);
+
+    return (
+        <div>랭킹페이지</div>
+    )
 }
 
 export default Ranking;
